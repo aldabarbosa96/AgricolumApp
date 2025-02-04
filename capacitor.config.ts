5c1ffba60ee8a6b2a99d8775bdd46de6c0046c42
aldabarbosa96 <aldabarbosa1996@gmail.com>
2025-02-04 18:33:27 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'agricolum',
+  webDir: 'dist'
+};
+
+export default config;
